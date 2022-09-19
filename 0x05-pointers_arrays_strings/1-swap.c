4f1fe624604dd42f5ec81efd08e5be2511616c92
Fiwasola <fiwasolajohn@gmgmail.com>
2022-09-19 15:55:58 -0700

    Pointers, arrays and strings tasks

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ *
+ * @a: int 1
+ * @b: int 2
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int foj;
+
+	foj = *a;
+	*a = *b;
+	*b = foj;
+}
