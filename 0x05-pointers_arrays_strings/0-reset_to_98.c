3613315d9b8bb1425d1f99f2b1b029eca0a7f14f
Fiwasola <fiwasolajohn@gmgmail.com>
2022-09-19 14:28:40 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * reset_to_98 -> a function to update the reference of  pointer
- * @n: pointer to n to be updated
+ * reset_to_98 - a function to update the value of point n to 98
+ *
+ * @n: input
+ * Return: void
  */
-void reset_to-98(int *n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,1 +1,1 @@
-Pointers, arrays and strings
+0x05. C Pointers, arrays and strings
